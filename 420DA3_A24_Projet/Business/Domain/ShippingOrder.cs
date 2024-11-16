da9e19d2ef1101449e4adc3d4c4396f125db75b5
Hajar-Chobani <hajar.chobani@lcieducation.net>
2024-11-16 21:51:36 +0100

    shippingOrder+Context

--- 420DA3_A24_Projet/Business/Domain/ShippingOrder.cs
+++ 420DA3_A24_Projet/Business/Domain/ShippingOrder.cs
@@ -1,9 +1,63 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using Project_Utilities.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _420DA3_A24_Projet.Business.Domain;
 public class ShippingOrder {
+    public int Id { get; set; }
+    public  ShippingOrderStatusEnum Status { get; set; }
+    public int SourceClientId { get; set; }
+    public int CreatorEmployeeTd {  get; set; }
+    public int DestinationAddressId { get; set; }
+    public int? FulfillEmployeedId { get; set; }
+    public DateTime? shippingDate { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime? DateModified { get; set; }
+    public DateTime? DateDeleted { get; set; }
+    public virtual client SourceClient { get; set; }
+    public virtual User CreatorEmplyee { get; set; }
+    public  virtual User? FulfillEmployee { get; set; }
+    public virtual Address DestinationAddress { get; set; }
+    public virtual List<ShippingOrderProduct> ShippingOrderProducts { get; set; } = new List<ShippingOrderProduct>();
+    
+    public void AssignToWhEmployee(User user)
+    {
+        FulfillEmployee = user;
+        FulfillEmployeedId = user.Id;
+        Status = ShippingOrderStatusEnum.Processing;
+
+    }
+    public void MarkPackaged() 
+    {
+        if (Status == ShippingOrderStatusEnum.Processing)
+        {
+            Status = ShippingOrderStatusEnum.Packaged;
+            DateModified = DateTime.Now;
+        } else
+        {
+            throw new InvalidOperationException("he order must be 'Processing");
+        }
+    }
+    public void MarkShipped()
+    {
+        if (Status == ShippingOrderStatusEnum.Packaged) 
+        {
+            Status = ShippingOrderStatusEnum.Shipped;
+            DateModified = DateTime.Now;
+            ShippingDate = DateTime.Now;
+        }
+        else 
+        {
+            throw new InvalidOperationException("Order cannot be marked as 'Shipped'");
+        }
+
+    }
+
+
 }
+
---
+++ 420DA3_A24_Projet/DataAccess/Contexts/AppDbContexte.cs
@@ -0,0 +1,29 @@
+﻿using _420DA3_A24_Projet.Business.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.DataAccess.Contexts;
+internal class AppDbContexte : DbContext {
+    public DbSet<ShippingOrder> ShippingOrders { get; set; }
+    public DbSet< Shipment> Shipments { get; set; }
+    public DbSet< ShipmentProduct > ShipmentProducts { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        base.OnConfiguring(optionsBuilder);
+
+        optionsBuilder.UseSqlServer("").UseLazyLoadingProxies();
+
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+
+    }
+
+}
