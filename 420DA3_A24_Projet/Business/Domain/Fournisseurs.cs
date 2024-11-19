e7132780c8268b01a47dc3ef32ac1a2a6a074df5
Akram Boughlala <boughlalaakram039@gmail.Com>
2024-11-19 14:30:47 -0500

    produit et fournisseurs

---
+++ 420DA3_A24_Projet/Business/Domain/Fournisseurs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static _420DA3_A24_Projet.Business.Domain.Produits;
+
+namespace _420DA3_A24_Projet.Business.Domain;
+internal class Fournisseurs {
+    public int Id { get; set; } 
+    public string Nom { get; set; }
+    public string NomContact { get; set; } 
+    public string PrenomContact { get; set; } 
+    public string EmailContact { get; set; } 
+    public string TelephoneContact { get; set; } 
+    public DateTime DateCreation { get; set; } 
+    public DateTime? DateModification { get; set; } 
+    public DateTime? DateSuppression { get; set; } 
+
+    public virtual List<Produit> Produits { get; set; } = new List<Produit>(); 
+
+    
+    public void AjouterProduit(Produit produit) {
+        if (produit != null) {
+            produit.FournisseurId = Id; 
+            Produits.Add(produit);
+        }
+    }
+
+    public void SupprimerProduit(Produit produit) {
+        Produits.Remove(produit);
+    }
+    public int CompterProduits() {
+        return Produits.Count; 
+    }
+    public void MettreAJourCoordonnees(string email, string telephone) {
+        EmailContact = email; 
+        TelephoneContact = telephone; 
+    }
+}
+
