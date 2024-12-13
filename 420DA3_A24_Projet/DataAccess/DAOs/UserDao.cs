219e8a2c0cc41eb45eb32371aedebfcfe0906bcc
Driss Laaziri <idrisslaaziri@gmail.com>
2024-12-13 00:59:44 -0500

    Dao

---
+++ 420DA3_A24_Projet/DataAccess/DAOs/UserDao.cs
@@ -0,0 +1,25 @@
+﻿using _420DA3_A24_Projet.Business.Domain;
+
+namespace _420DA3_A24_Projet.DataAccess.DAOs.Contracts;
+public interface IUserDao {
+
+    // +Create(User,password) : User
+    Task<User> CreateAsync(User user);
+
+    //+GetById(int) : User
+    Task<User?> GetByIdAsync(int id);
+
+    //+Update(User) : User
+    Task<User> UpdateAsync(User user);
+
+    // +Delete(User) : void
+    void Delete(User user);
+
+    // +Search(string) : List<User>
+    Task<IList<User>> SearchAsync(string pattern);
+
+    //+GetByWarehouse(Warehouse) : List<User>
+    Task<IList<User>> GetByWarehouseAsync(int warehouseId);
+    Task SetRoleAsync(int roleId, User user);
+    Task RemoveRoleAsync(int roleId, User user);
+}
